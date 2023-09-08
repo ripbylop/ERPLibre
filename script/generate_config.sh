ead4082bd760ae6c76c6761d228d42f638a62c03
Mathieu Benoit <mathben@technolibre.ca>
2023-09-08 15:58:25 -0400

    [UPD] generate_config

--- script/generate_config.sh
+++ script/generate_config.sh
@@ -28,6 +28,7 @@ printf "longpolling_port = ${EL_LONGPOLLING_PORT}\n" >> ${EL_CONFIG_FILE}
 printf "addons_path = ${EL_HOME_ODOO}/addons,${EL_HOME}/addons/addons," >> ${EL_CONFIG_FILE}
 printf "${EL_HOME}/addons/OCA_web," >> ${EL_CONFIG_FILE}
 if [[ ${EL_MINIMAL_ADDONS} = "False" ]]; then
+    printf "${EL_HOME}/addons/CybroOdoo_CybroAddons," >> ${EL_CONFIG_FILE}
     printf "${EL_HOME}/addons/Numigi_odoo-partner-addons," >> ${EL_CONFIG_FILE}
     printf "${EL_HOME}/addons/Numigi_odoo-web-addons," >> ${EL_CONFIG_FILE}
     printf "${EL_HOME}/addons/OCA_helpdesk," >> ${EL_CONFIG_FILE}
@@ -43,6 +44,7 @@ if [[ ${EL_MINIMAL_ADDONS} = "False" ]]; then
     printf "${EL_HOME}/addons/dhongu_deltatech," >> ${EL_CONFIG_FILE}
     printf "${EL_HOME}/addons/muk-it_muk_web," >> ${EL_CONFIG_FILE}
     printf "${EL_HOME}/addons/odoomates_odooapps," >> ${EL_CONFIG_FILE}
+    printf "${EL_HOME}/addons/tawasta_website," >> ${EL_CONFIG_FILE}
 fi
 printf "\n" >> ${EL_CONFIG_FILE}
 
